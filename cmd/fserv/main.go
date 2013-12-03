040230b76e73b1ebb7483d14d3cf518aae97bbb3
Ato Araki <ato.araki@gmail.com>
2013-12-03 18:50:37 +0900

    Add dir check

--- cmd/fserv/main.go
+++ cmd/fserv/main.go
@@ -1,25 +1,60 @@
 package main
 
 import (
-	"flag"
-	"log"
 	"net/http"
-	"strconv"
+
+	"errors"
+	"flag"
+	"os"
+
+	"fmt"
+	"log"
+	"path/filepath"
 )
 
-var portInt = flag.Int("port", 8000, "port number")
-var port = strconv.Itoa(*portInt)
-var dir = flag.String("d", "./", "server root")
+var addr = flag.String("addr", "127.0.0.1", "TCP network address")
+var port = flag.Int("port", 8000, "port number")
+var dir = flag.String("root", "./", "server root dir")
 
 func main() {
 	flag.Parse()
 
-	http.Handle("/", http.FileServer(http.Dir(*dir)))
+	setServerRoot("/", *dir)
 
-	log.Println("Starting server on:" + port)
-	err := http.ListenAndServe("0.0.0.0:"+port, nil)
+	addr := fmt.Sprintf("%s:%d", *addr, *port)
+	log.Printf("Starting server on: %s", addr)
 
+	err := http.ListenAndServe(addr, nil)
 	if err != nil {
 		log.Printf("Server failed: ", err.Error())
 	}
+}
+
+func setServerRoot(mount, dir string) {
+	err := mustDirExist(dir)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	path, err := filepath.Abs(dir)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("Server root: %s", filepath.ToSlash(path))
+
+	http.Handle(mount, http.FileServer(http.Dir(dir)))
+}
+
+func mustDirExist(dir string) error {
+	f, err := os.Stat(dir)
+	if os.IsNotExist(err) {
+		return errors.New(dir + ": No such directory")
+	}
+
+	if !f.IsDir() {
+		return errors.New(dir + ": Should be directory")
+	}
+
+	return nil
 }
